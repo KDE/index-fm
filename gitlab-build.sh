de3bbc5b063163f0c991f2be4b987355f258f4b6
Bruno Queiros <bqueiros@gmail.com>
2019-08-10 23:35:30 +0000

    Update gitlab-build.sh

--- gitlab-build.sh
+++ gitlab-build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo apt-get update; sudo apt-get upgrade -y ; sudo apt-get dist upgrade -y
 sudo apt-get update; sudo apt-get install sshpass linux-modules-$(uname -r) -y
 sudo apt-get install sshpass fuse -y ; sudo modprobe fuse 
 
@@ -12,5 +13,5 @@ chmod +x build-index-fm.sh
 
 export SSHPASS=$DEPLOY_PASS
 
-sshpass -e scp -q -o stricthostkeychecking=no *.AppImage $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
-#sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST 'bash /home/packager/repositories/nomad-desktop/repositories_util.sh'
+sshpass -e scp -q -o stricthostkeychecking=no /builds/nitrux/mauikit/index-fm/index-fm/build/*.AppImage $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH
+sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST 'bash /home/lnxslck/appimages/appimages_util.sh'
