6694d0565caebe418d12346f676caf8776d86b48
Anupam Basak <anupam.basak27@gmail.com>
2020-01-02 23:09:31 +0530

    Version 1.0.0

--- src/inx.h
+++ src/inx.h
@@ -5,6 +5,8 @@
 #include <QDebug>
 #include <QStandardPaths>
 
+#include "../index_version.h"
+
 namespace INX
 {
 Q_NAMESPACE
@@ -12,7 +14,7 @@ Q_NAMESPACE
 const QString NotifyDir = QStandardPaths::writableLocation(QStandardPaths::ConfigLocation);
 const QString appName = QStringLiteral("Index");
 const QString displayName = QStringLiteral("Index");
-const QString version = QStringLiteral("1.0.0");
+const QString version = QStringLiteral(INDEX_VERSION_STRING);
 const QString description = QStringLiteral("File manager");
 const QString orgName = QStringLiteral("Maui");
 const QString orgDomain = QStringLiteral("org.maui.index");
