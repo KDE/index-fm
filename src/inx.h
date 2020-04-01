f12363bc2b39535288968542cf8a408726cb1443
Camilo Higuita <chiguitar@unal.edu.co>
2020-04-01 17:19:35 -0500

    basic app info be static and const

--- src/inx.h
+++ src/inx.h
@@ -13,13 +13,13 @@ namespace INX
 {
 Q_NAMESPACE
 
-const QString NotifyDir = QStandardPaths::writableLocation(QStandardPaths::ConfigLocation);
-const QString appName = QStringLiteral("Index");
-const QString displayName = QStringLiteral("Index");
-const QString version = QStringLiteral(INDEX_VERSION_STRING);
-const QString description = QStringLiteral("File manager");
-const QString orgName = QStringLiteral("Maui");
-const QString orgDomain = QStringLiteral("org.maui.index");
+const static inline QString NotifyDir = QStandardPaths::writableLocation(QStandardPaths::ConfigLocation);
+const static inline QString appName = QStringLiteral("Index");
+const static inline QString displayName = QStringLiteral("Index");
+const static inline QString version = QStringLiteral(INDEX_VERSION_STRING);
+const static inline QString description = QStringLiteral("File manager");
+const static inline QString orgName = QStringLiteral("Maui");
+const static inline QString orgDomain = QStringLiteral("org.maui.index");
 }
 
 #endif // INX_H
