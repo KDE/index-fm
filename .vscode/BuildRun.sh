f724cbe89d484448179eefee32c7dc861cdc7c22
Gabriel Dominguez <gabriel@gabrieldominguez.es>
2020-10-06 22:43:30 +0200

    Enlaces simbolicos y BuildRun correcto

--- .vscode/BuildRun.sh
+++ .vscode/BuildRun.sh
@@ -1,2 +1,7 @@
-cd ../build/
+cd ../Maui/build
+#sudo rm -r *
+
+cmake -DCMAKE_INSTALL_PREFIX=/usr .. && make && make install
+
+cd ../../build/
 cmake -DCMAKE_INSTALL_PREFIX=/usr .. && make && ./bin/index
