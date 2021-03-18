f46c8f79320a8e55004d9037ec75eca2a45804cc
Camilo Higuita <chiguitar@unal.edu.co>
2021-03-17 22:43:40 -0500

    udpate build info label

--- src/main.cpp
+++ src/main.cpp
@@ -67,7 +67,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     KLocalizedString::setApplicationDomain("index");
 
     KAboutData about(QStringLiteral("index"), i18n("Index"), INDEX_VERSION_STRING, i18n("Index allows you to navigate, organize and preview your computer files."),
-                     KAboutLicense::LGPL_V3, i18n("© 2019-%1 Nitrux Development Team", QString::number(QDate::currentDate().year())), i18n("Built from ") + QString(GIT_BRANCH) + " " + QString(GIT_COMMIT_HASH));
+                     KAboutLicense::LGPL_V3, i18n("© 2019-%1 Nitrux Development Team", QString::number(QDate::currentDate().year())), QString(GIT_BRANCH) + "/" + QString(GIT_COMMIT_HASH));
 
     about.addAuthor(i18n("Camilo Higuita"), i18n("Developer"), QStringLiteral("milo.h@aol.com"));
     about.addAuthor(i18n("Gabriel Dominguez"), i18n("Developer"), QStringLiteral("gabriel@gabrieldominguez.es"));
