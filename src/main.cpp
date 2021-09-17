00e5b39e6b222008d9ac1afc0e1b9319339aefb2
Camilo Higuita <milo.h@aol.com>
2021-09-16 22:37:57 -0500

    update copyright

--- src/main.cpp
+++ src/main.cpp
@@ -63,7 +63,7 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
     KLocalizedString::setApplicationDomain("index");
 
     KAboutData about(QStringLiteral("index"), i18n("Index"), INDEX_VERSION_STRING, i18n("Browse, organize and preview your files."),
-                     KAboutLicense::LGPL_V3, i18n("© 2019-%1 Nitrux Development Team", QString::number(QDate::currentDate().year())), QString(GIT_BRANCH) + "/" + QString(GIT_COMMIT_HASH));
+                     KAboutLicense::LGPL_V3, i18n("© 2019-%1 Maui Development Team", QString::number(QDate::currentDate().year())), QString(GIT_BRANCH) + "/" + QString(GIT_COMMIT_HASH));
 
     about.addAuthor(i18n("Camilo Higuita"), i18n("Developer"), QStringLiteral("milo.h@aol.com"));
     about.addAuthor(i18n("Gabriel Dominguez"), i18n("Developer"), QStringLiteral("gabriel@gabrieldominguez.es"));
@@ -77,12 +77,6 @@ Q_DECL_EXPORT int main(int argc, char *argv[])
 
     QCommandLineParser parser;
     parser.setApplicationDescription(about.shortDescription());
-
-    if(about.setupCommandLine(&parser))
-    {
-        qDebug() << "App data was set correctly";
-    }
-
     parser.process(app);
     about.processCommandLine(&parser);
 
